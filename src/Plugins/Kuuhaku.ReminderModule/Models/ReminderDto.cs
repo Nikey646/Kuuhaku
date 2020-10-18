d01d7ba9f054b7289c879dba303a5b00ca1491d6
Nikey646 <nikey646@hotmail.com>
2020-10-18 22:30:45 +1100

    Missed a file

--- src/Plugins/Kuuhaku.ReminderModule/Models/ReminderDto.cs
+++ src/Plugins/Kuuhaku.ReminderModule/Models/ReminderDto.cs
@@ -1,17 +1,14 @@
 using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Kuuhaku.Database.DbModels
+namespace Kuuhaku.ReminderModule.Models
 {
-    public class Reminder
+    public class ReminderDto
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public Guid Id { get; set; }
-
-        public UInt64? GuildId { get; set; }
+        // Apart of the key
+        public UInt64 GuildId { get; set; }
         public UInt64 ChannelId { get; set; }
+        public UInt64 MessageId { get; set; }
+
         public UInt64 UserId { get; set; }
 
         public DateTime RemindAt { get; set; }
@@ -21,21 +18,20 @@ namespace Kuuhaku.Database.DbModels
 
         public DateTime CreatedAt { get; set; }
 
-        // EF Core ctor
-        public Reminder()
+        public ReminderDto()
         {
             this.CreatedAt = DateTime.UtcNow;
+            this.IsActive = true;
         }
 
-        public Reminder(UInt64? guildId, UInt64 channelId, UInt64 userId, DateTime when, String what) : this()
+        public ReminderDto(UInt64 guildId, UInt64 channelId, UInt64 messageId, UInt64 userId, DateTime when, String what) : this()
         {
             this.GuildId = guildId;
             this.ChannelId = channelId;
+            this.MessageId = messageId;
             this.UserId = userId;
             this.RemindAt = when;
             this.Contents = what;
-
-            this.IsActive = true;
         }
     }
 }
