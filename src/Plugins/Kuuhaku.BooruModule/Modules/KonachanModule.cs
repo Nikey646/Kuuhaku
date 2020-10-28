49170ec68172d905bd26ba65e29ec8e4d60574ec
Nikey646 <nikey646@hotmail.com>
2020-10-28 18:42:46 +1100

    Added KonaChan / Yandere subscribe commands.

--- src/Plugins/Kuuhaku.BooruModule/Modules/KonachanModule.cs
+++ src/Plugins/Kuuhaku.BooruModule/Modules/KonachanModule.cs
@@ -2,18 +2,23 @@ using System;
 using System.Threading.Tasks;
 using BooruViewer.Interop.Boorus;
 using BooruViewer.Interop.Interfaces;
+using Discord;
 using Discord.Commands;
 using Kuuhaku.BooruModule.Classes;
+using Kuuhaku.Commands.Attributes;
+using Kuuhaku.Commands.Models;
 
 namespace Kuuhaku.BooruModule.Modules
 {
     public class KonachanModule : GenericBooruModule
     {
+        private readonly SubscriptionService _service;
         public override IBooru Booru { get; }
 
         // ReSharper disable once SuggestBaseTypeForParameter
-        public KonachanModule(KonaChan booru, BooruRepository repository) : base(repository)
+        public KonachanModule(KonaChan booru, BooruRepository repository, SubscriptionService service) : base(repository)
         {
+            this._service = service;
             this.Booru = booru;
         }
 
@@ -21,6 +26,28 @@ namespace Kuuhaku.BooruModule.Modules
         public override Task GetPostsAsync(String tags = "")
         {
             return base.GetPostsAsync(tags);
+        }
+
+        [Command("Konachan subscribe"), RequiredMinPermission(CommandPermissions.Admin)]
+        public async Task SubscribeAsync(IChannel where = null)
+        {
+            where ??= this.Channel;
+
+            var subbed = await this._service.IsChannelSubscribed(this.Booru, this.Guild, where);
+            if (subbed)
+            {
+                await this._service.RemoveChannelAsync(this.Booru, this.Guild, where);
+                await this.ReplyAsync(
+                    this.Embed.WithDescription(
+                        $"You will no longer see new posts from KonaChan in {MentionUtils.MentionChannel(where.Id)}"));
+            }
+            else
+            {
+                await this._service.AddChannelAsync(this.Booru, this.Guild, where);
+                await this.ReplyAsync(
+                    this.Embed.WithDescription(
+                        $"You will now see new posts from KonaChan in {MentionUtils.MentionChannel(where.Id)}"));
+            }
         }
     }
 }
