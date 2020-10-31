41d4da86135138edd74bbae0dc0fde8502a785c1
Nikey646 <nikey646@hotmail.com>
2020-10-31 18:07:10 +1100

    Added support for systemd services on unix

--- src/Kuuhaku/Program.cs
+++ src/Kuuhaku/Program.cs
@@ -9,6 +9,7 @@ using Kuuhaku.Infrastructure.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Systemd;
 using Serilog;
 using StackExchange.Redis.Extensions.Core.Configuration;
 using StackExchange.Redis.Extensions.Newtonsoft;
@@ -44,7 +45,7 @@ namespace Kuuhaku
 
         private static IHostBuilder CreateHostBuilder(string[] args)
         {
-            return Host.CreateDefaultBuilder()
+            var builder = Host.CreateDefaultBuilder()
                 .ConfigureAppConfiguration((ctx, builder) =>
                 {
                     void AddEnvConfigs(IConfigurationBuilder b, params String[] prefixes)
@@ -118,7 +119,12 @@ namespace Kuuhaku
 
                     b.Token = ctx.Configuration[tokenKey];
                     b.SocketConfig = new DiscordSocketConfig {LogLevel = LogSeverity.Verbose, MessageCacheSize = 200,};
-                })
+                });
+
+            if (SystemdHelpers.IsSystemdService())
+                return builder.UseSystemd();
+
+            return builder
                 .UseConsoleLifetime();
         }
     }
